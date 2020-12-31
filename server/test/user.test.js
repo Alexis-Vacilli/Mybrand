bd60afba5dd8d317a147d6176559c914af51e027
Alexis-Vacilli <alexisvacilli1@gmail.com>
2020-12-31 11:15:18 +0200

    gitignore

--- server/test/user.test.js
+++ server/test/user.test.js
@@ -1,4 +1,4 @@
-import server from "../index";
+import server from "../../index";
 // import should from 'chai/register-should'
 
 import chai, { should } from "chai";
@@ -36,7 +36,7 @@ const mockUser = {
 
 describe('Login a user', async () => {
     it('should login a user', async  () => {
-        const res = await chai.request(server).post('/user/login').send(mockUser)
+        const res = await chai.request(server).post('/alex/login').send(mockUser)
         res.should.have.status(200)
     })
 
@@ -45,12 +45,12 @@ describe('Login a user', async () => {
             email:'blaise@gmail.com',
             password: 'bernarrd'
         }
-        const res = await chai.request(server).post('/user/login').send(mockuser)
+        const res = await chai.request(server).post('/alex/login').send(mockuser)
         res.should.have.status(401)
     })
 
     it('server error', async () => {
-        const res = await chai.request(server).post('/user/login').send()
+        const res = await chai.request(server).post('/alex/login').send()
         res.should.have.status(500)
     })
 })
